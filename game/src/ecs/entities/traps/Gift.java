18a68f7d3d93cb2c3c30749d4408e323478ad071
IntiGit <timo.schrage@fh-bielefeld.de>
2023-05-22 00:19:21 +0200

    Gift schadet Monstern

--- game/src/ecs/entities/traps/Gift.java
+++ game/src/ecs/entities/traps/Gift.java
@@ -2,8 +2,11 @@ package ecs.entities.traps;
 
 import dslToGame.AnimationBuilder;
 import ecs.components.AnimationComponent;
+import ecs.components.HealthComponent;
 import ecs.components.HitboxComponent;
 import ecs.components.PositionComponent;
+import ecs.damage.Damage;
+import ecs.damage.DamageType;
 import ecs.entities.Entity;
 import graphic.Animation;
 import level.elements.tile.Tile;
@@ -41,6 +44,12 @@ public class Gift extends Falle{
 
     @Override
     public void onCollision(Entity a, Entity b, Tile.Direction from) {
-
+        if (b != Game.getHero().get()) {
+            b.getComponent(HealthComponent.class)
+                .ifPresent(
+                    hc -> {
+                        ((HealthComponent) hc).receiveHit(new Damage(trapDmg, DamageType.NEUTRAL, null));
+                    });
+        }
     }
 }
